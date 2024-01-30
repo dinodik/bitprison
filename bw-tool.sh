3aed739db093cd3caabea7043f0de28b11f49943
dinodik <xw.louissun@gmail.com>
2024-01-30 16:18:19 +0000

    added looping on invalid inputs, branching for different requests, and a bit of aftercare by clearing the clipboard

--- bw-tool.sh
+++ bw-tool.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# TODO: run clear after timeout and wipe clipboard
+
 if [[ ! -v BW_SESSION ]]; then
   read -s -p "Master Password: "
-  BW_SESSION=$(echo $REPLY | bw --raw unlock 2>/dev/null) # stderr has `mac failed.`
-  echo $BW_SESSION
+  BW_SESSION=$(echo ${REPLY} | bw --raw unlock 2>/dev/null) # stderr has `mac failed.`
+  echo && echo Session ID: ${BW_SESSION}
   unset REPLY
 fi
 
@@ -12,8 +14,8 @@ do
   read -p "BW~$ " -a arr
   case ${arr[0]} in
     pass | card | name)
-      RAW=$(bw --raw list items --search ${arr[1]})
-      target/debug/bw-tool $RAW ${arr[0]}
+      RAW=$(bw --raw --session "${BW_SESSION}" list items --search "${arr[1]}")
+      target/debug/bw-tool "${RAW}" "${arr[0]}"
       ;;
     clear | c)
       clear
--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,108 @@ use std::{env, io};
 use std::process::Command;
 use json;
 
-// TODO 
-// - loop on index failure
-// - branch for pass/user/card inputs
-// - allow for continuation after copy (view other IDs or get different element of current)
-// - clear clipboard and terminal after timeout
+#[derive(Debug, Clone, Copy)]
+enum Request {
+    Password,
+    Username,
+    Card,
+}
+
+fn display_options(&request: &Request, i: usize, result: json::JsonValue) -> () {
+    let slice;
+    let statements = match request {
+        Request::Password => (&result["name"], &result["login"]["username"]),
+        Request::Username => (&result["name"], &result["login"]["username"]),
+        Request::Card => {
+            let s = &result["card"]["number"].as_str().expect("Invalid string found");
+            // get last four characters
+            let idx = s.char_indices().rev().nth(3).map(|(i, _)| i)
+                .expect("String too short");
+            slice = json::JsonValue::from(String::from(&s[idx..]));
+            (&result["name"], &slice)
+            },
+    };
+    println!("\t{i}:\t{}\t{}", &statements.0, &statements.1);
+}
+
+fn index_selection(n: usize) -> usize {
+    println!("Select an option [0-{}].", n - 1);
+    let index: usize;
+    let mut input = String::new();
+    loop {
+        input.clear();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        match input.trim().parse::<usize>() {
+            Ok(value) => {if value < n { index = value; break }},
+            Err(_) => {println!("Index entered is not valid.")}
+        }
+    }
+    index
+}
+
+fn process_output(&request: &Request, object: json::JsonValue) -> String {
+    let output = match request {
+        Request::Password => &object["login"]["password"],
+        Request::Username => &object["login"]["username"],
+        Request::Card => {
+            println!("Copying card number and displaying addtional info.");
+            println!("\tExpiry Date: {}/{}\tCVV: {}",
+                     &object["card"]["expMonth"],
+                     &object["card"]["expYear"],
+                     &object["card"]["code"]);
+            &object["card"]["number"]
+        },
+    };
+    output.to_string()
+}
+
+fn copy_to_clipboard(output: &str) {
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!("echo -n '{}' | xclip -selection clipboard", output))
+        .status()
+        .expect("Failed to copy to clipboard");
+    println!("Updated clipboard.");
+}
+
+fn after_care() -> () {
+     // - offer to clip pass after requesting user, or user after requesting pass
+     // - clear screen after?
+    println!("Press enter to clear clipboard.");
+    let _ = io::stdin().read_line(&mut String::new());
+    copy_to_clipboard("");
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let request = match &args[2] as &str {
+        "pass" => Request::Password,
+        "name" => Request::Username,
+        "card" => Request::Card,
+        &_ => panic!("Invalid request passed from bash script")
+    };
+
     let data = json::parse(&args[1]).expect("Unable to parse JSON");
-    println!("There are {} results for your search.", data.len());
-    for (i, result) in data.members().enumerate() {
-        println!("\t{i}:\t{}\t{}", &result["name"], &result["login"]["username"]);
+    let n = data.len();
+    if n == 0 {
+        println!("No results for your search.");
+        return
     }
-    println!("Select an option [0-{}].", data.len() - 1);
-    let mut index = String::new();
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read input");
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered is not a number");
-    let output: String = data.members()
-        .collect::<Vec<_>>()
-        .get(index)
-        .expect("Index out of bounds")["login"]["username"]
-        .to_string();
-    Command::new("sh")
-        .arg("-c")
-        .arg(format!("echo '{}' | xclip -selection clipboard", output))
-        .status()
-        .expect("Failed to copy to clipboard");
-    println!("{}", "Copied value into clipboard.");
+    println!("There are {} results for your search.", n);
+    for (i, result) in data.members().enumerate() {
+        display_options(&request, i, result.clone());
+    }
+
+    let index = match n {
+        1 => 0,
+        _ => index_selection(n),
+    };
+
+    let object = data.members().nth(index).unwrap();
+    let output = process_output(&request, object.clone());
+    copy_to_clipboard(&output);
+    after_care();
 }
