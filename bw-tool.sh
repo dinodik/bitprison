268aa99f1e9c8bf964e1433b42aac4a826defc57
dinodik <xw.louissun@gmail.com>
2024-01-30 16:28:46 +0000

    changed cargo project name to bw-tool-helper, and loop on master password failure

--- bw-tool.sh
+++ bw-tool.sh
@@ -2,12 +2,13 @@
 
 # TODO: run clear after timeout and wipe clipboard
 
-if [[ ! -v BW_SESSION ]]; then
+while [[ ! -v BW_SESSION || "$BW_SESSION" = "" ]]
+do
   read -s -p "Master Password: "
   BW_SESSION=$(echo ${REPLY} | bw --raw unlock 2>/dev/null) # stderr has `mac failed.`
   echo && echo Session ID: ${BW_SESSION}
   unset REPLY
-fi
+done
 
 while true
 do
@@ -15,7 +16,7 @@ do
   case ${arr[0]} in
     pass | card | name)
       RAW=$(bw --raw --session "${BW_SESSION}" list items --search "${arr[1]}")
-      target/debug/bw-tool "${RAW}" "${arr[0]}"
+      bw-tool-helper "${RAW}" "${arr[0]}"
       ;;
     clear | c)
       clear
